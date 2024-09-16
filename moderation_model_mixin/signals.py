4b1c056af5df7832dda7f6a00092244ce3cedcb1
Adriano <a.palumbo@frankhood.it>
2024-09-16 11:14:26 +0200

    rimozione providing-args

--- moderation_model_mixin/signals.py
+++ moderation_model_mixin/signals.py
@@ -4,5 +4,5 @@ from django.dispatch.dispatcher import Signal
 
 logger = logging.getLogger(__name__)
 
-set_accepted = Signal(providing_args=["instance", "explicit"])
-set_rejected = Signal(providing_args=["instance", "explicit"])
+set_accepted = Signal()
+set_rejected = Signal()
